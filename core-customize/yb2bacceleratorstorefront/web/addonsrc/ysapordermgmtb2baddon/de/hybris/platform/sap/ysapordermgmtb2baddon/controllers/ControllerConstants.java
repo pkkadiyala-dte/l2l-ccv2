c19098444d547a64581a1f70860de7879e6e0e46
“Pridhvi <pkadiyala@deloitte.com>
2020-03-24 23:05:34 -0400

    Renaming custom folder to core-customize

---
+++ core-customize/yb2bacceleratorstorefront/web/addonsrc/ysapordermgmtb2baddon/de/hybris/platform/sap/ysapordermgmtb2baddon/controllers/ControllerConstants.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.platform.sap.ysapordermgmtb2baddon.controllers;
+
+/**
+ *
+ */
+public interface ControllerConstants
+{
+	/**
+	 * Class with view name constants
+	 */
+	interface Views
+	{
+		interface Fragments
+		{
+			interface Cart
+			{
+				String AddToCartPopup = "fragments/cart/addToCartPopup";
+			}
+		}
+	}
+
+}
